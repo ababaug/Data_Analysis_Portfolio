04739371d0befd1947f0765e3d082381b1997431
Augustine Abah <ababaug@gmail.com>
2024-08-12 16:27:51 +0100

    updated web files

--- scripts.js
+++ scripts.js
@@ -1,8 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
     const menuToggle = document.getElementById('menu-toggle');
     const navMenu = document.getElementById('nav-menu');
+    const readMoreBtn = document.getElementById('read-more-btn');
+    const hiddenProjects = document.querySelectorAll('.project.hidden');
 
     menuToggle.addEventListener('click', function() {
         navMenu.style.display = navMenu.style.display === 'flex' ? 'none' : 'flex';
+    });
+
+
+    let isExpanded = false;
+
+    readMoreBtn.addEventListener('click', function() {
+        hiddenProjects.forEach(project => {
+            project.style.display = isExpanded ? 'none' : 'block';
+        });
+
+        readMoreBtn.textContent = isExpanded ? 'Read More' : 'Read Less';
+        isExpanded = !isExpanded;
     });
 });
